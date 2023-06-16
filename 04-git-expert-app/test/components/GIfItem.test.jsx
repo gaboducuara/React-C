1c9a3eddb785806b9485de92baccb3f9e3b3c9c5
gabriel <gabrielmancilladucuara@gmail.com>
2023-06-16 18:03:30 -0500

    actualizando datos

--- 04-git-expert-app/test/components/GIfItem.test.jsx
+++ 04-git-expert-app/test/components/GIfItem.test.jsx
@@ -1,7 +1,7 @@
-import { render } from "@testing-library/react";
-import { Gifitem } from "../../src/components/GIfItem";
+import { render, screen} from "@testing-library/react";
+import { Gifitem } from "../../src/components";
 
-describe('pruebas en <GIfItem/>', () => {
+describe('pruebas en <GIf Item/> en components', () => {
 
     const title = 'Goku';
     const url = 'http://goku.com/';
@@ -10,5 +10,14 @@ describe('pruebas en <GIfItem/>', () => {
 
         const { container } = render(<Gifitem title={ title } url={ url } />);
         expect( container ).toMatchSnapshot();
+    });
+
+    test ('debe de mostar la imagen con la URL Y con el ALT indicado' , () => {
+        render(<Gifitem title={ title } url={ url } />);
+
+        console.log(screen.getByRole('img').alt)
+        // const {src , alt } = screen.getByRole('img');
+        // expect(src).toBe(url);
+        // expect(alt).toBe(title);
     });
 });
