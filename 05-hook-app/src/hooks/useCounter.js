90c98bea8dee0c83122ecfc8f3371cca4efebf7a
gabriel <gabrielmancilladucuara@gmail.com>
2023-06-21 20:38:36 -0500

    datos

---
+++ 05-hook-app/src/hooks/useCounter.js
@@ -0,0 +1,37 @@
+//PARTE DE CUSTOM HOOKS
+
+//Esta parte es para realizar la logica del negocio
+// en este archivo se centra la logica
+
+import { useState } from "react"
+
+
+export const useCounter = ( initialValue = 48) => {
+
+    const [counter, setcounter] = useState( initialValue );
+
+
+    // incrementa +1 el contador
+    const increment = ( value = 1) => {
+        setcounter(counter + value);
+    };
+
+    // decrementa -1 el contador
+    const decrement = ( value = 1 ) => {
+        // choca contra el 0 y no baja mas
+        if( counter === 0 ) return;
+        setcounter(counter - value)
+    };
+
+    // Resetea el contador al estado inicial
+    const reset = () => {
+        setcounter (initialValue)
+    };
+
+    return {
+        counter , 
+        increment , 
+        decrement,
+        reset
+    };
+};
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks'
 // import { HooksApp } from './HooksApp'
-import { CounterApp } from './01-useState/CounterApp'
 // import { CounterApp } from './01-useState/CounterApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
-    <CounterApp />
+    {/* <CounterApp /> */}
+    <CounterWithCustomHooks />
   </React.StrictMode>,
 )
